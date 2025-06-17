d30fff8fdba3bff61206460c0d01a8abc448f5ba
KrishnanditoLksn <krislaksana123@gmail.com>
2025-06-17 21:18:15 +0700

    feat:fetch url code

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	// "somthing_golang/duplicate"
-	"os"
-	"somthing_golang/std_image"
+	// "os"
+	// "somthing_golang/std_image"
+	"somthing_golang/fetch_url"
 )
 
 func main() {
 	// duplicate.Duplicates()
-	std_image.Lissajous(os.Stdout)
+	// std_image.Lissajous(os.Stdout)
+	fetchurl.FetchUrl()
 }
